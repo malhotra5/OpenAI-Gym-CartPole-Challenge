7d0e8552a98ffcb12d7e271f96238de95b82d2be
Rohit <rohit@Alphonsos-MacBook-Air.local>
2019-06-14 12:54:38 -0700

    Training a model using a model

---
+++ ModelToModel/test.py
@@ -0,0 +1,44 @@
+import gym
+import tensorflow
+import time
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+import numpy as np
+
+
+env = gym.make("CartPole-v1")
+
+
+def model():
+	model = Sequential()
+	model.add(Dense(16, input_shape=(4,), activation="relu"))
+	model.add(Dense(12, activation="relu"))
+	model.add(Dense(4, activation="relu"))
+	model.add(Dense(2, activation="sigmoid"))
+	return model
+
+def testResults(modelPath):
+	observation = env.reset()
+	trainedModel = model()
+	trainedModel.load_weights(modelPath)
+
+	scoreTracker = 0
+
+	for i in range(200):
+	  env.render()
+	  action = trainedModel.predict(np.array([observation]))
+	  action = np.argmax(action)
+
+	  observation, reward, done, info = env.step(action)
+
+	  scoreTracker = scoreTracker + reward
+	  if done:
+	  	observation = env.reset()
+	  	print("Simulation performed with a score of {}".format(scoreTracker))
+	  	scoreTracker = 0
+
+
+
+testResults("newModel.hdf5")
---
+++ ModelToModel/train.py
@@ -0,0 +1,58 @@
+import gym
+import tensorflow
+import time
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+import numpy as np
+
+env = gym.make("CartPole-v1")
+
+
+def newModel():
+	model = Sequential()
+	model.add(Dense(16, input_shape=(4,), activation="relu"))
+	model.add(Dense(12, activation="relu"))
+	model.add(Dense(4, activation="relu"))
+	model.add(Dense(2, activation="sigmoid"))
+	return model
+
+def previousModel():
+	model = Sequential()
+	model.add(Dense(12, input_shape=(4,), activation="relu"))
+	model.add(Dense(8, activation="relu"))
+	model.add(Dense(2, activation="sigmoid"))
+
+	return model
+
+
+def trainFromPreviousModel(prevModel, nowModel):
+	nowModel.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+	
+	observation = env.reset()
+	for _ in range(1000):
+	  env.render()
+	  action = prevModel.predict(np.array([observation]))
+	  action = np.argmax(action)
+
+	  act = [0,0]
+	  act[action] = 1
+
+
+	  nowModel.fit(np.array([observation]), np.array([act]), epochs=1, batch_size=1, verbose=0)
+
+	  observation, reward, done, info = env.step(action)
+
+	  if done:
+	    observation = env.reset()
+
+	env.close()
+
+	nowModel.save("newModel.hdf5")
+
+prevModel = previousModel()
+prevModel.load_weights("previousMode.hdf5")
+trainFromPreviousModel(prevModel, newModel())
+
+
