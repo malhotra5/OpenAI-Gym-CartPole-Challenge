b696d3beb90bdde0f95aacc7c5d7023d13e6122a
Rohit <rohit@Alphonsos-MacBook-Air.local>
2019-06-14 12:02:28 -0700

    New approaches

---
+++ StreamingData/train.py
@@ -0,0 +1,43 @@
+import gym
+import tensorflow
+import time
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+import numpy as np
+
+
+env = gym.make("CartPole-v1")
+
+
+def model():
+	model = Sequential()
+	model.add(Dense(12, input_shape=(4,), activation="relu"))
+	model.add(Dense(8, activation="relu"))
+	model.add(Dense(2, activation="sigmoid"))
+
+	return model
+
+
+def streamTrain(model):
+	model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+	observation = env.reset()
+	for _ in range(1000):
+	  env.render()
+	  action = env.action_space.sample() # your agent here (this takes random actions)
+	 
+
+	  model.fit(np.array([observation]), to_categorical(np.array([action])), epochs=1, batch_size=1, verbose=0)
+
+	  observation, reward, done, info = env.step(action)
+
+	  if done:
+	    observation = env.reset()
+
+	env.close()
+
+	model.save("weights.hdf5")
+
+
+streamTrain(model())
